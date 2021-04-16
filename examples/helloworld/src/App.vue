8687f6a000c2defc29733d5c1853e3e4803fe78b
Hardian Jatmiko <eatmicco@hotmail.com>
2021-04-16 11:04:17 +0700

    Add example project.

---
+++ examples/helloworld/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <diagram :x="0" :y="0" />
+  </div>
+</template>
+
+<script>
+import Diagram from 'v-node-diagram';
+
+export default {
+  name: 'App',
+  components: {
+    Diagram
+  }
+}
+</script>
